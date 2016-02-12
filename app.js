a6c1524e8a031730cae4356088dac19e233fc77c
AmitaPatel3 <amitapatel3@hotmail.com>
2016-02-12 11:23:28 -0700

    added table to section 2

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-https://gist.github.com/fresh5447/32826da54ceb78c0491c
+var personOne = {firstName: "john", lastName: "johnson", phoneNumber: '404-333-3232'};
+var personTwo = {firstName: "sally", lastName: "johnson", phoneNumber: '404-334-2323'};
+var personThree = {firstName: "sue", lastName: "johnson", phoneNumber: '404-335-3223'};
+
+var people = [personOne, personTwo, personThree];
